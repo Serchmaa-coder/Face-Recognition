3c40b7c016c00e1aa69f68374272374ab59a3236
Maaleen <bolormaa7800@gmail.com>
2023-11-10 22:26:18 +0800

    mama

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,47 +1,50 @@
-'use client'
+import React from 'react';
 import { CameraOutlined, UserAddOutlined } from '@ant-design/icons';
 import styles from './page.module.css';
+import Image from './login.png'; // Ensure the path to your image is correct
 
-
-import React, { useState } from "react";
-export default function Login() {
+const LoginPage = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.con01}>
-          <button className={styles.btn01}>About</button>
-          <button className={styles.btn02}>Help</button>
+    <>
+      <header className={styles.header}>
+        <nav className={styles.navigation}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}><a href="/about">About</a></li>
+            <li className={styles.navItem}><a href="/help">Help</a></li>
+          </ul>
+        </nav>
+        <div className={styles.logo}>
+          Who's Using My Photo
         </div>
-        <div className={styles["title-section"]}>
-          <div className={styles.title01}>Who is using</div>
-          <div className={styles.title01}>    my photo?</div>
-          <div className={styles.con02}>
-            <CameraOutlined className={styles["title-icon"]} />
-          </div>   
+        <div className={styles.auth}>
+          <button className={styles.loginBtn}><CameraOutlined /> Login</button>
+          <button className={styles.signUpBtn}><UserAddOutlined /> Sign Up</button>
         </div>
-
-        <button className={styles["login-btn01"]}>Login</button>
-        <button className={styles.btn02}>Sign in</button>
+      </header>
+      <div className={styles.loginPage}>
+        <div className={styles.loginSection}>
+          <h1 className={styles.title}>HELLO!</h1>
+          <p className={styles.subtitle}>sign in your account</p>
+          <form className={styles.loginForm}>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="Username" className={styles.input} />
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="password" placeholder="Password" className={styles.input} />
+            </div>
+            <button type="submit" className={styles.loginButton}>LOGIN</button>
+          </form>
+        </div>
+        <div className={styles.imageSection}>
+          {/* If you're using Next.js, use the Image component from 'next/image' */}
+          <img src={Image.src} alt="Login" className={styles.loginImage} />
+        </div>
       </div>
-
-      <div>
-        <div className={styles.loginTitle01}>HELLO!</div>
-        <div className={styles.loginTitle02}>sign in your account</div>
-        </div>
-      <div className={styles.body}>
-        <UserAddOutlined className={styles.icon} />
-        <input className={styles["username-input"]} placeholder='Username'></input>
-        <input className={styles["password-input"]} placeholder='Password' type="password"></input>
-        <button className={styles["login-btn02"]}>LOGIN</button>
-      </div>
-    <div>
-        <div className={styles.footer}>© 2023 SeraMaa, Inc. All Rights Reserved. Do Not Sell Or Share My Personal Information</div>
-
-    </div>
-
-    </div>
-    
+      <footer className={styles.footer}>
+        © 2023 SeraMaa, Inc. All Rights Reserved. Do Not Sell Or Share My Personal Information
+      </footer>
+    </>
   );
+};
 
-}
-
+export default LoginPage;
